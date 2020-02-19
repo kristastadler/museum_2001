077a7ae2ab615d605be0b45c27b762b370405403
“Krista <stadler.krista@gmail..com>
2020-02-19 09:25:30 -0700

    Set up museum test file and wrote test it exists

---
+++ test/museum_test.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/patron'
+require './lib/exhibit'
+require './lib/museum'
+
+class MuseumTest < Minitest::Test
+
+  def test_it_exists
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    assert_instance_of Museum, dmns
+  end
+
+
+
+end
