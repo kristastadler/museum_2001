1cfd18873859bf35147843c319d2e1098f771513
“Krista <stadler.krista@gmail..com>
2020-02-19 09:19:31 -0700

    Passed test it has attributes

--- lib/patron.rb
+++ lib/patron.rb
@@ -1,8 +1,12 @@
 class Patron
+  attr_reader :name,
+              :spending_money
+  attr_accessor :interests
 
   def initialize(name, spending_money)
     @name = name
     @spending_money = spending_money
+    @interests = []
   end
 
-end     
+end
--- test/patron_test.rb
+++ test/patron_test.rb
@@ -7,5 +7,15 @@ class PatronTest < Minitest::Test
   def test_it_exists
     patron_1 = Patron.new("Bob", 20)
 
-    assert_instance_of Patron, patron1
-  end   
+    assert_instance_of Patron, patron_1
+  end
+
+  def test_it_has_attributes
+    patron_1 = Patron.new("Bob", 20)
+
+    assert_equal "Bob", patron_1.name
+    assert_equal 20, patron_1.spending_money
+    assert_equal [], patron_1.interests
+  end
+
+end
