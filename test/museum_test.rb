ea097175b1427984a9e5a3b017686f4bec9c5190
“Krista <stadler.krista@gmail..com>
2020-02-19 09:28:05 -0700

    Wrote test it has attributes

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -12,6 +12,12 @@ class MuseumTest < Minitest::Test
     assert_instance_of Museum, dmns
   end
 
+  def test_it_has_attributes
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    assert_equal "Denver Museum of Nature and Science", dmns.name
+    assert_equal [], dmns.exhibits 
+  end
 
 
 end
